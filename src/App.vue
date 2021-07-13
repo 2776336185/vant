a17022fa3215bc4d21c713ecb310b166dbf3fa51
zhanghaihu <2776336185@qq.com>
2021-07-13 20:43:11 +0800

    tr

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,30 @@
 <template>
   <div id="app">
-    <h2>ll</h2>
-    <router-view/>
+    <h1>axios的使用</h1>
+    <button @click="getReq()">发起get请求</button>
+    <router-view />
   </div>
 </template>
 <script>
+import axios from 'axios'
 export default {
-  name: 'App'
+  setup () {
+    const getReq = () => {
+      axios
+        .get(
+          'http://demo.itlike.com/web/xlmc/api/homeApi/categoriesdateil/lk001'
+        )
+        .then((res) => {
+          console.log(res.data)
+        })
+        .catch((error) => {
+          console.log(error)
+        })
+    }
+    return {
+      getReq
+    }
+  }
 }
 </script>
 <style lang="less">
---
+++ vue.config.js
@@ -0,0 +1,4 @@
+module.exports = {
+  // 去除eslint校验
+  lintOnSave: false
+}
